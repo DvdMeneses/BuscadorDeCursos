727b3472550c21cd70245d5137a570775987d724
DvdMeneses <david.meneses.cc@gmail.com>
2024-06-26 21:18:59 -0300

    Primeiro commit

---
+++ Testes/TestBuscadorDeCursos.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Dvdmeneses\BuscadorDeCurso\Tests;
+
+use Dvdmeneses\BuscadorDeCurso\Buscador;
+use GuzzleHttp\ClientInterface;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
+use Symfony\Component\DomCrawler\Crawler;
+
+class TestBuscadorDeCursos extends TestCase
+{
+    private $httpClientMock;
+    private $url = 'url-teste';
+
+    protected function setUp(): void
+    {
+        $html = <<<FIM
+        <html>
+            <body>
+                <span class="card-curso__nome">Curso Teste 1</span>
+                <span class="card-curso__nome">Curso Teste 2</span>
+                <span class="card-curso__nome">Curso Teste 3</span>
+            </body>
+        </html>
+        FIM;
+        echo $html; // Verifique se o HTML está correto
+
+        $stream = $this->createMock(StreamInterface::class);
+        $stream
+            ->method('getContents')
+            ->willReturn($html);
+
+        $response = $this->createMock(ResponseInterface::class);
+        $response
+            ->method('getBody')
+            ->willReturn($stream);
+
+        $httpClient = $this->createMock(ClientInterface::class);
+        $httpClient
+            ->method('request')
+            ->with('GET', $this->url)
+            ->willReturn($response);
+
+        $this->httpClientMock = $httpClient;
+    }
+
+    public function testBuscadorDeveRetornarCursos()
+    {
+        $crawler = new Crawler();
+        $buscador = new Buscador($this->httpClientMock, $crawler);
+        $cursos = $buscador->buscar($this->url);
+
+        print_r($cursos); // Verifique se os cursos foram extraídos corretamente
+
+        $this->assertCount(3, $cursos);
+        $this->assertEquals('Curso Teste 1', $cursos[0]);
+        $this->assertEquals('Curso Teste 2', $cursos[1]);
+        $this->assertEquals('Curso Teste 3', $cursos[2]);
+    }
+}
